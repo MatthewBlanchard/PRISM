efeaa0dc9d05fceed9e7a51cdb26b2af92a4f2ed
Liam <liamreadsspam@gmail.com>
2020-06-12 11:16:04 -0300

    Fix bug when unwielding

--- actions/unwield.lua
+++ actions/unwield.lua
@@ -7,7 +7,7 @@ Unwield.targets = {targets.Unwield}
 function Unwield:perform(level)
   local weapon = self:getTarget(1)
 
-  self.owner.wielded = nil
+  self.owner.wielded = self.owner.defaultAttack
 end
 
 return Unwield
