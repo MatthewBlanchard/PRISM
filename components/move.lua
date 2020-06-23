96908270780e4a44e67e41d04f486e253394c0d8
Matthew Blanchard <matthewrblanchard@gmail.com>
2020-06-23 15:22:04 -0400

    Added move speed to move component and sqeeter sprite.

--- components/move.lua
+++ components/move.lua
@@ -2,8 +2,15 @@ local Component = require "component"
 
 local Move = Component:extend()
 
+function Move:__new(speed)
+  self.speed = speed
+end
+
 function Move:initialize(actor)
-  actor:addAction(actions.Move)
+  local moveAction = actions.Move:extend()
+  moveAction.time = self.speed or 100
+
+  actor:addAction(moveAction)
   actor.passable = false
 end
 
