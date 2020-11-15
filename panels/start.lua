e230f92bbe3686d0e1fa9f13236d84ce84f42837
Matthew Blanchard <matthewblanc@msn.com>
2020-11-14 23:24:07 -0500

    Improved start screen.

--- panels/start.lua
+++ panels/start.lua
@@ -6,9 +6,16 @@ function Start:__new(display)
   Panel.__new(self, display, parent)
 end
 
+function Start:update(dt)
+  self.time = self.time or 0
+  self.time = self.time + dt
+end
 function Start:draw()
-  self.display:clear(nil, nil, nil, nil, nil, nil, {0, 0, 0})
-  self.display:writeCenter("Below the Garden", math.floor(self.h / 2), {0.5, 0.5, 0.7}, {0, 0, 0})
+  local t = math.min(1, self.time / 4)
+  local tC = 0.9 * t
+  self.display:clear(nil, nil, nil, nil, nil, nil, {.09, .09, .09})
+  self.display:writeCenter("Below the Garden", math.floor(self.h / 2)-10, {tC, tC, tC}, {.09, .09, .09})
+  self.display:writeCenter("Press any key to begin.", math.floor(self.h / 2), {tC, tC, tC}, {.09, .09, .09})
 end
 
 function Start:handleKeyPress(keypress)
